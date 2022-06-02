69c0bcc70ae009d32f8168a8bff24426c38d26ad
Cahyo Susilo <cahyosusilo775@gmail.com>
2022-06-02 11:06:36 +0700

    first commit

---
+++ Assignment.java
@@ -0,0 +1,14 @@
+public class Assignment{
+  public static void main(String args[]){
+    /*
+    Buatlah aplikasi untuk menampilkan angka 1-50, ketika angka n habis dibagi 3 maka keluar tulisan "n habis dibagi 3", dan jika n habis dibagi 5 maka keluar tulisan "n habis dibagi 5"
+    */
+    for(int n = 1; n <= 50; n++){
+      if(n % 3 == 0){
+        System.out.println(n + " habis dibagi 3");
+      }else if(n % 5 == 0){
+        System.out.println(n + " habis dibagi 5");
+      }
+    }
+  }
+}
